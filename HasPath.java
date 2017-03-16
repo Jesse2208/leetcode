c3c3c14b52de1c685b77441947bba45a81f035c8
Jesse2208 <756652408@qq.com>
2017-03-16 15:36:21 +0800

    矩阵中的路径
    请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。路径可以从矩阵中的任意一个格子开始，每一步可以在矩阵中向左，向右，向上，向下移动一个格子。如果一条路径经过了矩阵中的某一个格子，则该路径不能再进入该格子。
    例如[a b c e s f c s a d e
    e]是3*4矩阵，其包含字符串"bcced"的路径，但是矩阵中不包含“abcb”路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能再次进入该格子。

---
+++ HasPath.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class HasPath {
+	public boolean hasPath(char[] matrix, int rows, int cols, char[] str){
+		int[][]flag=new int[rows][cols];
+		int index=0;
+		for(int i=0;i<rows;i++)
+			Arrays.fill(flag[i],0);
+		for(int i=0;i<rows;i++)
+			for(int j=0;j<cols;j++){
+				if(tell(matrix,flag,str,i,j,rows,cols,index))
+					return true;
+				else for(int k=0;k<rows;k++)
+						Arrays.fill(flag[k],0);
+			}
+		return false;
+			
+    }
+	public boolean tell(char[] matrix,int[][]flag,char[] str,int i,int j,int rows, int cols,int index){
+		if(i>=0&&j>=0&&i<rows&&j<cols&&flag[i][j]==0){
+			if(matrix[i*cols+j]==str[index]){
+				if(index==str.length-1)
+					return true;
+				else{
+					flag[i][j]=1;
+					return 	tell(matrix,flag,str,i-1,j,rows,cols,index+1)||tell(matrix,flag,str,i+1,j,rows,cols,index+1)||tell(matrix,flag,str,i,j+1,rows,cols,index+1)||tell(matrix,flag,str,i,j-1,rows,cols,index+1);
+				}
+			}
+			else return false;
+		}
+		else return false;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HasPath h=new HasPath();
+		char[]matrix={'a','b','c','e','s','f','c','s','a','d','e','e'};
+		int rows=3,cols=4;
+		char[]str={'b','c','c','e','d'};
+		char[]str2={'a','b','c','b'};
+		System.out.println(h.hasPath(matrix, rows, cols, str2));
+	}
+
+}
